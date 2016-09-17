5b5341d3fcbe6b471c10d4fc929a189aa08e91eb
Matthew Butler <matthewtbutler@gmail.com>
2016-09-16 23:20:57 -0500

    timer function

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let check = require('./lib/parser.js');
 let _ = require('lodash');
-let round = 0;
+round = 0;
 let numOfPlayers = 0;
+let players = [];
+let leftHand = ["W", "W", "S", "W", "P", "P", "F", "P", "P", "P", "D", "S", "S", "S", "F", "P", "W", "P", "F", "D"];
+let rightHand = ["P", "S", "F", "W", "P", "S", "D", "F", "P"];
 
 let playerTemplate = {
 	name: '',
@@ -10,21 +13,55 @@ let playerTemplate = {
 	leftHand: [],
 	rightHand: [],
 	hp: 15,
-	protection: [],
+	protection: ["fart"],
 	enchantment: [],
 	resistance: [],
 	monsters: [],
-	timer: []
+	timer: {}
 };
 
+
+
 // START GAME
+
+//round 0, initialization
 let player1 = _.cloneDeep(playerTemplate);
+players.push(player1);
 player1.name = "Korath";
-console.log(player1.name);
+player1.rightHand = rightHand;
+
+//round 1
+
+let right_hand_spells = check(player1.rightHand);
+var first_rh_spell = right_hand_spells[0].action(player1);
+console.log(right_hand_spells[0].name);
+console.log(first_rh_spell);
+console.log(player1);
+
+
+//run timer functions at end of round. forEach through instead of first index
+runTimer(player1, 3);
+
+
+console.log(player1);
+
+
 // END GAME
 
-let leftHand = ["W", "W", "S", "W", "P", "P", "F", "P", "P", "P", "D", "S", "S", "S", "F", "P", "W", "P", "F", "D"];
-let rightHand = ["P", "S", "F", "W", "P", "S", "D", "F"];
+//executes all the functions stored for the specified player's round
+function runTimer(player, round) {
+	if (player.timer[round].length > 0) {
+		_.forEach(player.timer, function(value, key) {
+			_.forEach(value, function(v, k) {
+				v();
+			})
+		})
+	}
+}
 
-console.log(check(rightHand));
+
+
+
+
+
 
