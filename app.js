12b667359a1d70fc0ecf24f419194707d2005e61
Matthew Butler <matthewtbutler@gmail.com>
2016-09-16 19:44:00 -0500

    fix parser
    change to correct direction. add basic character sheet

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-var check = require('./lib/parser.js');
+let check = require('./lib/parser.js');
+let _ = require('lodash');
+let round = 0;
+let numOfPlayers = 0;
 
-var leftHand = ["W", "W", "S", "W", "P", "P", "F", "P", "P", "P", "D", "S", "S", "S", "F", "P", "W", "P", "F", "D"];
-var rightHand = ["P", "P", "P", "W", "W", "F", "S", "P"];
+let playerTemplate = {
+	name: '',
+	currentGame: '',
+	skinColor: '',
+	leftHand: [],
+	rightHand: [],
+	hp: 15,
+	protection: [],
+	enchantment: [],
+	resistance: [],
+	monsters: [],
+	timer: []
+};
+
+// START GAME
+let player1 = _.cloneDeep(playerTemplate);
+player1.name = "Korath";
+console.log(player1.name);
+// END GAME
+
+let leftHand = ["W", "W", "S", "W", "P", "P", "F", "P", "P", "P", "D", "S", "S", "S", "F", "P", "W", "P", "F", "D"];
+let rightHand = ["P", "S", "F", "W", "P", "S", "D", "F"];
 
 console.log(check(rightHand));
 
