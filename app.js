2f56c04e4adc8865e64e3e56bea6de95230bc054
Matthew Butler <matthewtbutler@gmail.com>
2016-09-17 00:14:22 -0500

    takeTurn and getSpells
    begin to automate taking turn and returning spell list to display

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ round = 0;
 let numOfPlayers = 0;
 let players = [];
 let leftHand = ["W", "W", "S", "W", "P", "P", "F", "P", "P", "P", "D", "S", "S", "S", "F", "P", "W", "P", "F", "D"];
-let rightHand = ["P", "S", "F", "W", "P", "S", "D", "F", "P"];
+let rightHand = ["D", "S"];
 
 let playerTemplate = {
 	name: '',
@@ -29,34 +29,56 @@ let player1 = _.cloneDeep(playerTemplate);
 players.push(player1);
 player1.name = "Korath";
 player1.rightHand = rightHand;
+player1.leftHand = leftHand;
 
 //round 1
+round++;
+takeTurn(player1, "F", ">");
+let spellList = getSpells(player1);
+console.log(spellList);
 
-let right_hand_spells = check(player1.rightHand);
-var first_rh_spell = right_hand_spells[0].action(player1);
-console.log(right_hand_spells[0].name);
-console.log(first_rh_spell);
-console.log(player1);
+
+//let first_rh_spell = right_hand_spells[0].action(player1);
+//console.log(right_hand_spells[0].name);
+//console.log(first_rh_spell);
+//console.log(player1);
 
 
 //run timer functions at end of round. forEach through instead of first index
-runTimer(player1, 3);
-
-
-console.log(player1);
-
+runTimer(player1, round);
 
 // END GAME
 
 //executes all the functions stored for the specified player's round
 function runTimer(player, round) {
-	if (player.timer[round].length > 0) {
+	if (player.timer[round] !== undefined) {
 		_.forEach(player.timer, function(value, key) {
 			_.forEach(value, function(v, k) {
 				v();
 			})
 		})
 	}
+}
+
+function takeTurn(player, right, left) {
+	player.rightHand.push(right);
+	player.leftHand.push(left);
+}
+
+function getSpells(player) {
+	let spells = [];
+	let right_hand_spells = check(player1.rightHand);
+	let left_hand_spells = check(player1.leftHand);
+
+	if (right_hand_spells !== undefined) {
+		spells = _.concat(spells, right_hand_spells);
+	}
+
+	if (left_hand_spells !== undefined) {
+		spells = _.concat(spells, left_hand_spells)
+	}
+
+	return spells;
 }
 
 
